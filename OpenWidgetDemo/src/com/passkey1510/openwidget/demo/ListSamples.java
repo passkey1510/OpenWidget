1f54c2c84e2d0960f5dedfd4fb5078721d9417e9
Kieu Anh Tuan <passkey1510@gmail.com>
2012-03-20 01:17:42 +0100

    Add License notice

--- OpenWidgetDemo/src/com/passkey1510/openwidget/demo/ListSamples.java
+++ OpenWidgetDemo/src/com/passkey1510/openwidget/demo/ListSamples.java
@@ -1,6 +1,24 @@
-/**
+/*
  * This activity intelligently retrieves every demo activity under the demo package and put them in a List
+ * Taken from package com.viewpagerindicator.sample of Jake Wharton
  */
+
+/*
+ * Copyright (C) 2011 Jake Wharton
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.passkey1510.openwidget.demo;
 
 import java.text.Collator;
